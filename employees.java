b78976910bd120fdae53a94e3801259a15eb0642
Sidhant1225 <80540426+Sidhant1225@users.noreply.github.com>
2021-07-29 22:50:06 +0530

    Add files via upload

---
+++ employees.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class employees
+{
+public static void main(String args[])
+{
+int n,i,j;
+Scanner kb=new Scanner(System.in);
+System.out.println("Enter value of n");
+n=kb.nextInt();
+String a[][]=new String[n][]
+}
+}
